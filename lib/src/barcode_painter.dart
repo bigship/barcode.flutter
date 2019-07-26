bigship/barcode.flutter

commit 2483ec4020c467a3777190b0b1edbd6fb684c176

fix invalid ean8 barcode checksum algorithm

lib/src/barcode_painter.dart
@@ -568,7 +568,12 @@ class BarCodePainter extends CustomPainter {
 
     sum2nd = st.getUint8(6) + st.getUint8(4) + st.getUint8(2) + st.getUint8(0);
     sum3rd = st.getUint8(5) + st.getUint8(3) + st.getUint8(1);
-    checkCode = 10 - (sum2nd*3+sum3rd) % 10;
+    if ((sum2nd + sum3rd * 3) % 10 == 0) {
+      checkCode = 0;
+    } else {
+      checkCode = 10 - (sum2nd + sum3rd * 3) % 10;
+    }
+
     for (int i = 0; i < 7; i++) {
       Rect rect = new Rect.fromLTWH(64*lineWidth+i*lineWidth, 0.0, lineWidth, height);
       ((0x40 & (codeC[checkCode] << i)) == 0x40) ? painter.color = Colors.black : painter.color = Colors.white;

commit c46d36eb100c6e7ddfe9e43b1de0d584a72a2110

fix invalid checksum bug introduced by 2483ec4020c467a3777190b0b1edbd6fb684c176

lib/src/barcode_painter.dart
@@ -568,10 +568,10 @@ class BarCodePainter extends CustomPainter {
 
     sum2nd = st.getUint8(6) + st.getUint8(4) + st.getUint8(2) + st.getUint8(0);
     sum3rd = st.getUint8(5) + st.getUint8(3) + st.getUint8(1);
-    if ((sum2nd + sum3rd * 3) % 10 == 0) {
+    if ((sum3rd + sum2nd * 3) % 10 == 0) {
       checkCode = 0;
     } else {
-      checkCode = 10 - (sum2nd + sum3rd * 3) % 10;
+      checkCode = 10 - (sum3rd + sum2nd * 3) % 10;
     }
 
     for (int i = 0; i < 7; i++) {